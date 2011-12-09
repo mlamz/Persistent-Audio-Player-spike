cb4d70bb906a13769945c08728a55342b0d97a0c
michael <michael.lam@7digital.com>
2011-12-09 16:03:38 +0000

    initial mvc3 scaffolding with a couple of pages

---
+++ PersistentAudioPlayerSpike/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace PersistentAudioPlayerSpike.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult AnotherPage()
+		{
+			return View();
+		}
+
+    }
+}
